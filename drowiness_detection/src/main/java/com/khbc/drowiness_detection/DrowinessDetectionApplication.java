b0a349f21e09cb35b672b5a37b0473bdde98c5c9
ThanhfVux2102 <24560017@gm.uit.edu.vn>
2025-11-05 22:03:27 +0700

    feat: structure for backend

---
+++ drowiness_detection/src/main/java/com/khbc/drowiness_detection/DrowinessDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.khbc.drowiness_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrowinessDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrowinessDetectionApplication.class, args);
+	}
+
+}
---
+++ drowiness_detection/src/test/java/com/khbc/drowiness_detection/DrowinessDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khbc.drowiness_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrowinessDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
