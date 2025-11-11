d46af85fb438e92cfcacaae9b569df5233a3f127
Kaso45 <phamcam102016@gmail.com>
2025-11-11 21:39:40 +0700

    fix: project package typo

---
+++ drowsiness_detection/src/main/java/com/khbc/drowsiness_detection/DrowsinessDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.khbc.drowsiness_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrowsinessDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrowsinessDetectionApplication.class, args);
+	}
+
+}
---
+++ drowsiness_detection/src/test/java/com/khbc/drowiness_detection/DrowinessDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khbc.drowsiness_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class drowsinessDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
